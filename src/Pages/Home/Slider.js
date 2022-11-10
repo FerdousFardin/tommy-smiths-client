f7c75a1b4b368baf7ca93023dffde1895e846b8b
FerdousFardin <fardinh39u@gmaile.com>
2022-11-10 23:13:43 +0600

    slider updated

--- src/Pages/Home/Slider.js
+++ src/Pages/Home/Slider.js
@@ -42,13 +42,13 @@ export const Slider = () => {
           <span className="relative text-teal-900">Gallery</span>
         </span>
       </h2>
-      <p className=" absolute top-2 lg:top-5 right-20 w-1/3 mb-6 text-sm md:text-base text-right text-gray-700 lg:text-lg">
+      <p className="hidden lg:inline-block absolute top-5 right-20 w-1/3 mb-6 text-base text-right text-gray-700">
         I wanted to tell a story on the street. While often in the magazines the
         act of walking is portrayed in a stereotyped and frankly boring way,
         this is where looking to the future is for me, this is where the future
         is for me.
       </p>
-      <div className="px-20 bg-slate-200">
+      <div className="sm:px-2 md:px-5 lg:px-20 bg-slate-200">
         <div className="carousel py-20 relative w-full h-screen">
           {sliders.map((slideData, idx) => (
             <SliderItems key={idx} slideData={slideData} />
